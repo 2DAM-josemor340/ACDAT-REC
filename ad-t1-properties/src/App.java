0de7be480f36db92a9094fb063f87de5489de2ab
2DAM-josemor340 <jmorvel291@g.educaand.es>
2025-03-20 16:13:12 +0100

    Update App.java

--- ad-t1-properties/src/App.java
+++ ad-t1-properties/src/App.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Introducir la ruta del fichero : ");
-        String ruta;
-        ruta = sc.nextLine();
+         Scanner sc = new Scanner(System.in);
+        System.out.print("Introducir el nombre del Archivo : ");
+        String archivo;
+        archivo = sc.nextLine();
+        String ruta = "res" + File.separator + archivo;
         try {
             File file = new File(ruta);
             if (file.exists()) {
@@ -15,9 +16,31 @@ public class App {
 
                 GestorProperties gestor = new GestorProperties(file);
 
-                System.out.println(gestor.LeerPropiedad("username"));
+                System.out.println("Estan son las propiedades del archivo properties "+file.getName()+" , seleccione la propiedad que desee ver su valor");
 
+                gestor.mostrarPropiedades();
 
+                String seleccion;
+
+                do {
+                    System.out.println("Seleccione el valor que desea ver o presione N para salir:");
+                    seleccion = sc.nextLine();
+
+                    if (!seleccion.equalsIgnoreCase("N")) {
+                        String propiedad = gestor.leerPropiedad(seleccion);
+
+                        if (propiedad != null) {
+                            System.out.println(seleccion + " = " + propiedad);
+                        } else {
+                            System.out.println("La propiedad '" + seleccion + "' no existe.");
+                            gestor.mostrarPropiedades();
+                        }
+                    }
+
+                } while (!seleccion.equalsIgnoreCase("N"));
+
+                System.out.println("Adiós");
+                sc.close();
 
             } else {
                 System.out.println("Fichero NO existe");
